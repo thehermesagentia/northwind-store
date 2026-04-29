cfea8ac31b26302d3c2e9c5053267232a9af9483
burakorkmez <burakorkmezz@gmail.com>
2026-04-29 12:20:50 +0800

    feat: implement product catalog with seeding script, routing, and UI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Show, SignInButton, SignUpButton, useAuth, UserButton } from "@clerk/react";
 import PageLoader from "./components/PageLoader";
 import Layout from "./components/Layout";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
 
 function App() {
   const { isLoaded } = useAuth();
@@ -9,20 +11,9 @@ function App() {
 
   return (
     <Layout>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-
-      <p className="text-red-500 font-extrabold text-4xl bg-blue">Hello</p>
-      <button className="btn btn-primary">Click me</button>
-      <button className="btn btn-secondary">Click me</button>
-      <button className="btn btn-outline">Click me</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </Layout>
   );
 }
